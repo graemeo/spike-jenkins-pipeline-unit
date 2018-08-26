e34f0d1d9c2e58470cc4e7c0a991e38cb4c2c4b0
Graeme O <graeme.ong>
2018-08-26 18:43:06 +1000

    [GraemeO] Update test

--- src/test/groovy/SpikeTest.groovy
+++ src/test/groovy/SpikeTest.groovy
@@ -1,4 +1,11 @@
+import com.lesfurets.jenkins.unit.global.lib.GitSource
+import org.assertj.core.api.Assertions
+
+import static com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration.library
+
 import com.lesfurets.jenkins.unit.BasePipelineTest
+import com.lesfurets.jenkins.unit.MethodCall
+
 import org.junit.Before
 import org.junit.Test
 
@@ -17,6 +24,31 @@ class SpikeTest extends BasePipelineTest
 
         script.call()
         printCallStack()
+    }
+
+    @Test
+    void shouldLoadJenkinsfileWithSharedLibrary() {
+        String clonePath = "out"
+        def library = library()
+                        .name("hello")
+                        .retriever(GitSource.gitSource("git@github.com:graemeo/spike-jenkins-pipeline-unit.git"))
+                        .targetPath(clonePath)
+                        .defaultVersion("master")
+                        .allowOverride(true)
+                        .implicit(false)
+                        .build()
+
+        helper.registerSharedLibrary(library)
+
+        runScript("src/test/jenkins/jenkinsfileWithSharedLibrary")
+
+        Assertions.assertThat(helper.callStack.findAll { call ->
+            call.methodName == "sh"
+        }.any { call ->
+            MethodCall.callArgsToString(call).contains("echo 'goodbye world'")
+        }).isTrue()
+
+        assertJobStatusSuccess()
     }
 
 }
